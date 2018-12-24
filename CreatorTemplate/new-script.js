8884b7ce8d57966774fc40524e906a87430ce012
leo <907600065@qq.com>
2018-12-24 16:19:07 +0800

    修改New-script模板

--- CreatorTemplate/new-script.js
+++ CreatorTemplate/new-script.js
@@ -1,60 +1,39 @@
-
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
+  properties: {
 
-    },
+  },
 
-    /**
-     * 构造函数,不能在此操作this.node
-     * 只能做变量初始化，不建议使用
-     */
-    ctor() {
+  onLoad() {
+    this.registerEvent();
+  },
 
-    },
+  registerEvent() {
 
-    /**
-     * 组件脚本的初始化,可以操作this.node
-     */
-    onLoad() {
+  },
 
-    },
+  init(data) {
 
-    /**
-     * 注册事件
-     */
-    registerEvent() {
-    },
+  },
 
-    /**
-     * 使用cc.instantiate()创建实例时
-     * 通过getComponent(脚本名称)取得脚本实例
-     * 然后使用init(data)来传递参数
-     * @param {*} data 
-     */
-    init(data) {
+  update(dt) {
 
-    },
+  },
 
-    /**
-     * 通常用于初始化一些中间状态的数据
-     */
-    start() {
+  onEnable() {
+    this.registerEvent();
+  },
 
-    },
+  onDisable() {
+    this.offEvent();
+  },
 
-    /**
-     * 每一帧回调
-     * @param {*} dt 
-     */
-    update(dt) {
+  offEvent() {
+    cc.emitter.offThis(this);
+  },
 
-    },
+  onDestroy() {
 
-    /**
-     * 统一回收组件
-     */
-    onDestroy() {
-    }
+  }
 });
