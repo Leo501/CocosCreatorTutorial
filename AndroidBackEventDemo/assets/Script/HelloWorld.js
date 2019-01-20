a895e570e342e987b5ef2e8cd14536a714db3527
我是leo <907600065@qq.com>
2019-01-20 18:20:53 +0800

    添加一个 android 返回键 demo

---
+++ AndroidBackEventDemo/assets/Script/HelloWorld.js
@@ -0,0 +1,52 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        label: {
+            default: null,
+            type: cc.Label
+        },
+        text: 'Hello, World!'
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        this.label.string = this.text;
+        this.registerEvent();
+    },
+
+    registerEvent() {
+        //android 返回键
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    },
+
+    onKeyDown(event) {
+        switch (event.keyCode) {
+            case cc.KEY.back:
+                if (this.isBackGame) {
+                    cc.director.end();
+                    return;
+                }
+                this.isBackGame = true;
+                this.label.string = '再次按返回键\n将退出游戏';
+                this.node.runAction(cc.sequence(cc.delayTime(3), cc.callFunc(() => {
+                    this.isBackGame = false;
+                    this.label.string = '';
+                })));
+                break;
+        }
+    },
+
+    offEvent() {
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+
+    onDestroy() {
+        this.offEvent();
+    }
+});
